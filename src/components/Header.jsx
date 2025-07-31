e6ecb1a6cfff08bc4ce2da3f243d0e58548d1f29
Azizbek16l <azizbektopilboyev7@gmail.com>
2025-07-31 16:12:07 +0500

    main added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,18 @@
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container">
+        <h2>Bello Olange</h2>
+        <ul>
+          <li><a href="#Home">Home</a></li>
+          <li><a href="#Product">Product</a></li>
+          <li><a href="#Faq">Faq</a></li>
+          <li><a href="#Contact">Contact</a></li>
+
+        </ul>
+      </div>
+    </header>
   )
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,19 @@
 
 function Main() {
   return (
-    <div>Main</div>
+   <main>
+    <div className="container">
+      <h2>Food app</h2>
+      <h1>Why stay hungry when 
+you can order form Bella Onojie</h1>
+      <p>Download the bella onoje’s food app now on</p>
+      <div className="btns">
+      <button id="playstore">Playstore</button>
+      <button id="appstore">App store</button> </div>
+
+    </div>
+    <div className="backdrop"></div>
+   </main>
   )
 }
 
