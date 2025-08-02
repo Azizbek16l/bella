7236c6fda55fbf8a9efa1043664c0760fcfc01c7
Azizbek16l <azizbektopilboyev7@gmail.com>
2025-08-02 16:01:11 +0500

    added section 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Main from './components/Main'
+import Section from './components/Section'
 function App() {
   return (
     <>
       <Header/>
       <Main/>
+      <Section/>
       <Footer/>
     </>
   )
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,7 +2,14 @@
 
 function Section() {
   return (
-    <div>Section</div>
+    <section>
+      <div className="container">
+        <img  id="phone" src="https://i.ibb.co/Kxw2sQQB/phone.png"  width={650} />
+
+        <span id="line"></span>
+        <h2>How the app works</h2>
+      </div>
+    </section>
   )
 }
 
